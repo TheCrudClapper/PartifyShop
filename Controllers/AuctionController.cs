1778cb2226688021f8e4d9d5816f4b82b92e0946
VortexOoN <wojciechmucha12@gmail.com>
2025-03-18 01:27:09 +0100

    -All current views are responsive -Cleared CSS styles mistakes, conflicts -Finished page for displaying one offer

--- Controllers/AuctionController.cs
+++ Controllers/AuctionController.cs
@@ -12,5 +12,9 @@ namespace ComputerServiceOnlineShop.Controllers
         {
             return View();
         }
+        public IActionResult ShowAuction()
+        {
+            return View();
+        }
     }
 }
